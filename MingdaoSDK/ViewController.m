a51823be80729dd68c2a3f4c879db8b3e2666b32
WeeTom <lovinghmy@gmail.com>
2013-06-18 09:29:07 +0800

    Readme

--- MingdaoSDK/ViewController.m
+++ MingdaoSDK/ViewController.m
@@ -22,8 +22,6 @@
 }
 
 - (IBAction)loginBtnPressed:(id)sender {
-    [MDAPIManager setAppKey:@"935527504"];
-    [MDAPIManager setAppSecret:[[UIDevice currentDevice] uniqueGlobalDeviceIdentifier]];
 #warning setAppkey and setAppSecet before use
     /*
      @must
@@ -45,41 +43,8 @@
                                                     NSLog(@"%@", error.userInfo);
                                                     NSLog(@"%@", [error.userInfo objectForKey:@"error"]);
                                                 }
-                                                
-                                                /*[[[MDAPIManager sharedManager] loadAllVotessWithPageIndex:0 pagesize:0 handler:^(NSArray *votes, NSError *error){
-                                                    for (MDPost *p in votes) {
-                                                        NSLog(@"%@", p.text);
-                                                    }
-                                                }] start];*/
-                                                /*[[[MDAPIManager sharedManager] createImagePostWithText:@"API TEST" image:[UIImage imageNamed:@"Default-568h@2x.png"] groupIDs:nil shareType:3 handler:^(NSString *pID, NSError *error){
-                                                    NSLog(@"%@", pID);
-                                                    [[[MDAPIManager sharedManager] createRepostWithText:@"repost" postID:pID groupIDs:nil shareType:3 handler:^(NSString *rID, NSError *error){
-                                                        NSLog(@"%@", rID);
-                                                    }] start];
-                                                }] start];*/
-                                                /*[[[MDAPIManager sharedManager] createTaskWithTaskName:@"APITeST" description:nil endDateString:@"2013-6-7" chargerID:nil memberIDs:nil projectID:nil handler:^(NSString *tID, NSError *error){
-                                                    NSLog(@"%@", tID);
-                                                    [[[MDAPIManager sharedManager] createTaskReplymentOnTaskWithTaskID:tID message:@"test" replyToReplymentWithRID:nil image:[UIImage imageNamed:@"Default-568h@2x.png"] handler:^(NSString *trID, NSError *error){
-                                                        NSLog(@"%@", trID);
-                                                    }] start];
-                                                }] start];*/
-                                                /*[[[MDAPIManager sharedManager] loadCurrentUserJoinedTasksWithKeywords:nil allOrUnfinished:YES handler:^(NSArray *tasks, NSError *error){
-                                                    for (MDTask *t in tasks) {
-                                                        NSLog(@"%@", t.objectName);
-                                                        [[[MDAPIManager sharedManager] loadTaskWithTaskID:t.objectID handler:^(MDTask *t, NSError *error){
-                                                            for (MDUser *u in t.members) {
-                                                                NSLog(@"%@", u.objectName);
-                                                                [[[MDAPIManager sharedManager] loadTaskReplymentsWithTaskID:t.objectID maxID:nil pageSize:20 handler:^(NSArray *rs, NSError *error){
-                                                                    for (MDTaskReplyment *r in rs) {
-                                                                        NSLog(@"%@", r.text);
-                                                                    }
-                                                                }] start];
-                                                            }
-                                                        }] start];
-                                                        break;
-                                                    }
-                                                }] start];*/
                                             }];
+    
     // use start when you are ready to request, it will not start automatically
     [c start];
     // use cancel if you want to stop
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 api_ios
 =======
+明道Objective-C 版本的SDK，包含了如何用户鉴权和常用的API调用
+
+此版本基于Objective-C 2.0 ARC，工程项目请用Xcode 4.6或者更高版本打开，对应iOS版本为5.0或更高
+
+调用API请修改配置文件的AppKey和AppSecret内容 公有云请到明道开放平台创建应用 http://open.mingdao.com 私有部署请联系管理员创建应用并获取令牌
+
+具体开发指南请见《明道开放平台指南》
