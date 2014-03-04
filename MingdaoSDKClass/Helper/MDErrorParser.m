de9d783d691f4d64cdb6a6803aef70dbfb3f8d0c
WeeTom <lovinghmy@gmail.com>
2014-03-04 16:32:12 +0800

    update

--- MingdaoSDKClass/Helper/MDErrorParser.m
+++ MingdaoSDKClass/Helper/MDErrorParser.m
@@ -14,76 +14,81 @@
     NSInteger errorCodeInt = [errorCode integerValue];
     switch (errorCodeInt) {
         case 10001:
-            return @"缺少参数";
+            return NSLocalizedString(@"缺少参数", @"缺少参数");
         case 10002:
-            return @"参数所带值错误";
+            return NSLocalizedString(@"参数所带值错误", @"参数所带值错误");
         case 10003:
-            return @"非法操作";
+            return NSLocalizedString(@"非法操作", @"非法操作");
         case 10004:
-            return @"您没有权限查看此群组";
+            return NSLocalizedString(@"您没有权限查看此群组", @"您没有权限查看此群组");
         case 10005:
-            return @"内部错误";
+            return NSLocalizedString(@"内部错误", @"内部错误");
         case 10006:
-            return @"您没有权限查看此任务";
+            return NSLocalizedString(@"您没有权限查看此任务", @"您没有权限查看此任务");
         case 10007:
-            return @"请求数据不存在";
+            return NSLocalizedString(@"请求数据不存在", @"请求数据不存在");
         case 10008:
-            return @"您没有权限查看此日程";
+            return NSLocalizedString(@"您没有权限查看此日程", @"您没有权限查看此日程");
         case 10009:
-            return @"无权对任务进行操作(权限仅给任务负责人或任务创建者)";
+            return NSLocalizedString(@"无权对任务进行操作(权限仅给任务负责人或任务创建者)", @"无权对任务进行操作(权限仅给任务负责人或任务创建者)");
         case 10010:
-            return @"无权对日程进行操作(权限仅给日程创建者)";
+            return NSLocalizedString(@"无权对日程进行操作(权限仅给日程创建者)", @"无权对日程进行操作(权限仅给日程创建者)");
         case 10011:
-            return @"无权对群组进行操作(权限仅给群组管理员或网络管理员)";
+            return NSLocalizedString(@"无权对群组进行操作(权限仅给群组管理员或网络管理员)", @"无权对群组进行操作(权限仅给群组管理员或网络管理员)");
         case 10012:
-            return @"无权对动态进行操作(权限仅给动态创建者或网络管理员)";
+            return NSLocalizedString(@"无权对动态进行操作(权限仅给动态创建者或网络管理员)", @"无权对动态进行操作(权限仅给动态创建者或网络管理员)");
         case 10013:
-            return @"无权查看动态相关内容";
+            return NSLocalizedString(@"无权查看动态相关内容", @"无权查看动态相关内容");
         case 10014:
-            return @"无权查看群组相关内容";
+            return NSLocalizedString(@"无权查看群组相关内容", @"无权查看群组相关内容");
             
         case 10101:
-            return @"请求令牌不存在";
+            return NSLocalizedString(@"请求令牌不存在", @"请求令牌不存在");
         case 10102:
-            return @"请求令牌签名不合法";
+            return NSLocalizedString(@"请求令牌签名不合法", @"请求令牌签名不合法");
         case 10103:
-            return @"用户账号不存在";
+            return NSLocalizedString(@"用户账号不存在", @"用户账号不存在");
         case 10104:
-            return @"登录验证失败,密码错误";
+            return NSLocalizedString(@"登录验证失败,密码错误", @"登录验证失败,密码错误");
         case 10105:
-            return @"用户访问令牌失效";
+            return NSLocalizedString(@"用户访问令牌失效", @"用户访问令牌失效");
         case 10107:
-            return @"由于多次尝试失败,账号已被锁定,20分钟后解锁";
+            return NSLocalizedString(@"由于多次尝试失败,账号已被锁定,20分钟后解锁", @"由于多次尝试失败,账号已被锁定,20分钟后解锁");
             
         case 10201:
-            return @"Email不合法";
+            return NSLocalizedString(@"Email不合法", @"Email不合法");
         case 10202:
-            return @"Email已经注册";
+            return NSLocalizedString(@"Email已经注册", @"Email已经注册");
         case 10203:
-            return @"非本网络Email";
+            return NSLocalizedString(@"非本网络Email", @"非本网络Email");
         case 10204:
-            return @"非法邀请";
+            return NSLocalizedString(@"非法邀请", @"非法邀请");
         case 10205:
-            return @"有效域名邮箱，不能通过来宾邀请加入";
+            //TODO: TRANSLATE
+            return NSLocalizedString(@"有效域名邮箱，不能通过来宾邀请加入", @"有效域名邮箱，不能通过来宾邀请加入");
         case 10206:
-            return @"无权限邀请来宾";
+            return NSLocalizedString(@"无权限邀请来宾", @"无权限邀请来宾");
             
         case 10301:
-            return @"暂无应用的最新版本";
+            return NSLocalizedString(@"暂无应用的最新版本", @"暂无应用的最新版本");
         case 10302:
-            return @"仅限高级模式的管理员，针对企业应用";
+            //TODO: TRANSLATE
+            return NSLocalizedString(@"仅限高级模式的管理员，针对企业应用", @"仅限高级模式的管理员，针对企业应用");
         case 10303:
-            return @"仅限高级模式的管理员调用";
+            //TODO: TRANSLATE
+            return NSLocalizedString(@"仅限高级模式的管理员调用", @"仅限高级模式的管理员调用");
         case 10304:
-            return @"仅限高级模式可调用";
+            //TODO: TRANSLATE
+            return NSLocalizedString(@"仅限高级模式可调用", @"仅限高级模式可调用");
         case 10305:
-            return @"仅限应用创建者可调用";
+            //TODO: TRANSLATE
+            return NSLocalizedString(@"仅限应用创建者可调用", @"仅限应用创建者可调用");
 
         case 10401:
-            return @"扩展应用未安装";
+            return NSLocalizedString(@"扩展应用未安装", @"扩展应用未安装");
             
         default:
-            return @"操作失败";
+            return NSLocalizedString(@"操作失败", @"操作失败");
     }
 }
 
