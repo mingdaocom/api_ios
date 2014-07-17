332687ae12b59ba0d7b20776fc6e92dc74cb2dc8
WeeTom <lovinghmy@gmail.com>
2014-07-17 11:13:00 +0800

    should Merge to Master

--- MingdaoSDKClass/API/MDAPIManager.m
+++ MingdaoSDKClass/API/MDAPIManager.m
@@ -47,7 +47,7 @@ static MDAPIManager *sharedManager = nil;
 {
     if (!_serverAddress) {
         _serverAddress = MDAPIDefaultServerAddress;
-        _serverAddress = @"http://172.16.23.247/MD.api.Web";
+        //_serverAddress = @"http://172.16.23.247/MD.api.Web";
         return _serverAddress;
     }
     return _serverAddress;
