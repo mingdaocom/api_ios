8eaa276e865e80f60580a2be0eef1ae485b0d844
WeeTom <lovinghmy@gmail.com>
2015-09-30 14:05:50 +0800

    use url session replace url connection

--- MingdaoSDKClass/Helper/MDURLConnection.h
+++ MingdaoSDKClass/Helper/MDURLConnection.h
@@ -3,7 +3,7 @@
 //  Mingdao
 //
 //  Created by Wee Tom on 13-4-26.
-//
+//  Refactored by Wee Tom on 15-9-30
 //
 
 #import <Foundation/Foundation.h>
@@ -12,14 +12,37 @@ extern NSString *MDURLConnectionErrorOccurred;
 
 @class MDURLConnection;
 typedef void (^MDAPIHandler)(MDURLConnection *theConnection, id jsonObjectOrRawDataIfError, NSError *error);
-typedef void (^MDAPICGFloatHandler)(float fValue);
 
 @interface MDURLConnection : NSObject
 - (MDURLConnection *)initWithRequest:(NSURLRequest *)request handler:(MDAPIHandler)handler;
-@property (assign, nonatomic) BOOL errorNotification; // default: Yes
+
+/**
+ *  use custom session to change task rule, set before call start or else use [NSURLSession sharedSession]
+ */
+@property (strong, nonatomic) NSURLSession *session;
+
+/**
+ *  post notification when error occur, default to YES
+ */
+@property (assign, nonatomic) BOOL errorNotification;
+
+/**
+ *  current request
+ */
 @property (readonly, nonatomic) NSURLRequest *request;
+
+/**
+ *  timeout default to 30
+ */
 @property (assign, nonatomic) NSTimeInterval timeOut;
-@property (copy, nonatomic) MDAPICGFloatHandler downloadProgressHandler, uploadProgressHandler;
+
+/**
+ * start the task, call the handler when completion or error occurred
+ */
 - (void)start;
+
+/**
+ *  cancel the task without calling the handler
+ */
 - (void)cancel;
 @end
