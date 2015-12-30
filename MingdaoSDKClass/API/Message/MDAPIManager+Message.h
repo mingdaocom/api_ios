a9b6b1b4ec3e6d3aeb7b912ed41cbcfb3a751343
WeeTom Wang <lovinghmy@gmail.com>
2015-12-30 10:21:54 +0800

    delete unused messages

--- MingdaoSDKClass/API/Message/MDAPIManager+Message.h
+++ MingdaoSDKClass/API/Message/MDAPIManager+Message.h
@@ -10,15 +10,6 @@
 
 @interface MDAPIManager (Message)
 #pragma mark - 私信接口
-
-/*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-
- @usage:
- 获取当前登录用户私人消息列表（列举所有私信的发送人列表,按照最新消息时间排序）
- @parmas:
- handler - 处理获取到的包含多个MDMessageAll的NSArray
- -*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-- (MDURLConnection *)loadCurrentUserMessagesWithHandler:(MDAPINSArrayHandler)handler __deprecated;
-
 /*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-
  @usage:
  获取当前登录用户与其它单个用户的私人消息列表
@@ -32,37 +23,5 @@
                                    pageSize:(NSInteger)size
                                        page:(NSInteger)pages
                                     handler:(MDAPINSArrayHandler)handler;
-
-/*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-
- @usage:
- 向某个用户发送一条私人消息
- @parmas:
- userID   - 私信来自这个userID的user
- text     - 私信内容
- handler  - 处理发送成功后返回的私信ID
- -*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-- (MDURLConnection *)sendMessageToUserID:(NSString *)userID
-                                 message:(NSString *)text
-                                  images:(NSArray *)images
-                                 handler:(MDAPINSStringHandler)handler;
-
-/*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-
- @usage:
- 删除当前登录用户的私人消息
- @parmas:
- mID      - 被删除的消息编号
- handler  - 处理删除的结果
- -*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-- (MDURLConnection *)deleteMessageWithMessageID:(NSString *)mID handler:(MDAPIBoolHandler)handler;
-
-/*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-
- @usage:
- 标记某条消息已经阅读
- @parmas:
- mID      - 被阅读的消息编号
- handler  - 处理已读的结果
- -*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-- (MDURLConnection *)markMessageAsReadWithMessageID:(NSString *)mID handler:(MDAPIBoolHandler)handler;
-
 
 @end
